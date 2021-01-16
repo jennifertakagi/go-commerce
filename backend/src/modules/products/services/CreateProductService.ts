efb0a9efdca56e4c79a40a99a40c68e387586cfe
jennifertakagi <jennitakagi@gmail.com>
2021-01-16 17:56:37 -0300

    feat: create Product module

---
+++ backend/src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Product from '../infra/typeorm/entities/Product';
+import IProductsRepository from '../repositories/IProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+@injectable()
+class CreateProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const findProductByName = await this.productsRepository.findByName(name);
+
+    if (findProductByName) {
+      throw new AppError('There is a product with the same email!');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
+  }
+}
+
+export default CreateProductService;
